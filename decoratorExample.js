1ef2ad6baac31f84c570d9ecc540b58a9ac62b54
Mateusz Milewski <milewski.88@gmail.com>
2018-06-19 08:38:50 +0200

    ts and js files

---
+++ decoratorExample.js
@@ -0,0 +1,84 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.getDescription = function () {
+        return this.description;
+    };
+    return Car;
+}());
+var ModelS = /** @class */ (function (_super) {
+    __extends(ModelS, _super);
+    function ModelS() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.description = "Model S";
+        return _this;
+    }
+    ModelS.prototype.cost = function () {
+        return 73000;
+    };
+    return ModelS;
+}(Car));
+var ModelX = /** @class */ (function (_super) {
+    __extends(ModelX, _super);
+    function ModelX() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.description = "Model X";
+        return _this;
+    }
+    ModelX.prototype.cost = function () {
+        return 77000;
+    };
+    return ModelX;
+}(Car));
+var CarOptions = /** @class */ (function (_super) {
+    __extends(CarOptions, _super);
+    function CarOptions() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return CarOptions;
+}(Car));
+var EnhancedAutoPilot = /** @class */ (function (_super) {
+    __extends(EnhancedAutoPilot, _super);
+    function EnhancedAutoPilot(c) {
+        var _this = _super.call(this) || this;
+        _this.decoratedCar = c;
+        return _this;
+    }
+    EnhancedAutoPilot.prototype.getDescription = function () {
+        return this.decoratedCar.getDescription() + ", Enhanced AutoPilot";
+    };
+    EnhancedAutoPilot.prototype.cost = function () {
+        return this.decoratedCar.cost() + 5000;
+    };
+    return EnhancedAutoPilot;
+}(CarOptions));
+var RearFacingSeats = /** @class */ (function (_super) {
+    __extends(RearFacingSeats, _super);
+    function RearFacingSeats(c) {
+        var _this = _super.call(this) || this;
+        _this.decoratedCar = c;
+        return _this;
+    }
+    RearFacingSeats.prototype.getDescription = function () {
+        return this.decoratedCar.getDescription() + ", Rear facing seats";
+    };
+    RearFacingSeats.prototype.cost = function () {
+        return this.decoratedCar.cost() + 4000;
+    };
+    return RearFacingSeats;
+}(CarOptions));
+var myTesla = new ModelS();
+myTesla = new RearFacingSeats(myTesla);
+myTesla = new EnhancedAutoPilot(myTesla);
+console.log(myTesla.cost());
+console.log(myTesla.getDescription());
